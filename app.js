1d9f862c036d67c8a7465af40de1da3e9cc805ae
LABORDE <dylan4047@gmail.com>
2016-10-21 09:32:30 +0200

    get value from days years and mouth with check button

--- app.js
+++ app.js
@@ -6,41 +6,43 @@ console.log("hello world");
 		value : undefined,
 		init : function(){
 			app.watcher();
-			app.mois();
-
+			
 		},
-
 		watcher : function(){
 			var that = this ;
 			$("#btCheck").on("click",this.check.bind(this));
-			$("#userInputDays").on("click", this.days.bind(this));
-			$("#userInputYears").on("click", this.years.bind(this));
-			$("#selectMois").on("click", this.mois.bind(this));
+			$("#userInputDays").on("change", this.days.bind(this));
+			$("#userInputYears").on("change", this.years.bind(this));
+			$("#selectMois").on("change", this.mois.bind(this));
 		},
 		check : function(){
-			console.log("lalal");
+		// console.log(moment("this.jours, this.mois, this.an" ,"DD-MM-YYYY").format("dddd"));
+		console.log(this.jours,this.mois,this.an);
+		
 		},
+		
 
 		days: function(){
-
+			this.jours = $("#userInputDays").val();
+			
 		},
 
 		years : function(){
+			this.an = $("#userInputYears").val();
 
 		},
 
 		mois: function(){
-			console.log($("#selectMois").val());
+			this.mois =$("#selectMois").val();
 
 		},
 
 
-
-
 	}
 
-
+// moment("2016/11/12", "YYYY-MM-DD").format("dddd");
 
 
 app.init();
 })();
+		// console.log(moment("this.an this.jours", "YYYY-DD").format("llll"));
