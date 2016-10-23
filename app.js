ab41959507fcba76aff99d49aa09d993892ab4c9
LABORDE <dylan4047@gmail.com>
2016-10-24 01:29:59 +0200

    add condition for empty or wrong user entry

--- app.js
+++ app.js
@@ -9,12 +9,12 @@ console.log("hello world");
 		
 		init : function(){
 			app.watcher();
-			 this.month();
+			this.month();
 			console.log(this.jours);
 			console.log(this.jours)
 			$("#message").hide();
 
-			   this.remove();
+			this.remove();
 			
 		},
 		watcher : function(){
@@ -23,16 +23,19 @@ console.log("hello world");
 			$("#userInputDays").on("change", this.days.bind(this));
 			$("#userInputYears").on("change", this.years.bind(this));
 			$("#selectMois").on("change", this.month.bind(this));
-			$("#restart").on("click", this.remove.bind(this));
+			$("#restart").on("click", this.restart.bind(this));
 		},
 		check : function(){	
-	var userdate = moment([this.jours+this.mois+this.an],"DD-MM-YYYY")
+			var userdate = moment([this.jours+this.mois+this.an],"DD-MM-YYYY")
 			var date = userdate.format("dddd");
-			
-		 	 $("#main").hide()
-		 	 $("#overlay").show();
+			$("#main").hide()
+			$("#overlay").show();
 			$("#msgOverlay").append(date);
-			 this.condition();
+			$("#message").hide();
+			$("#userInputDays").css("border-color","#47b8e0");
+			$("#userInputYears").css("border-color","#47b8e0");
+			this.condition();
+			console.log(date);
 		},
 		
 
@@ -49,9 +52,21 @@ console.log("hello world");
 
 		},
 		condition: function(){
+			if (this.jours === undefined) {
+				this.conditionDays();
+			}
+			if (this.an === undefined) {
+				this.conditionYears();	
+			};
+			if (this.an === "") {
+				this.conditionYears();	
+			};
 			if (this.jours === undefined && this.an === undefined) {
 				console.log("haha");
+				$("#userInputDays").css("border-color","#ff7473");
+				$("#userInputYears").css("border-color","#ff7473");
 				$("#message").show();
+				$("#msg").html("");
 				$("#msg").append("erreur aucune valeur n'a été renseigné");
 				$("#main").show();
 				$("#overlay").hide();
@@ -59,26 +74,49 @@ console.log("hello world");
 			
 			
 		},
-		remove: function(){
-			 $("#main").show();
+		remove : function(){
+			$("#main").show();
 			$("#msgOverlay").html("");			
-			 $("#overlay").hide();
-			console.log("lala");
-
+			$("#overlay").hide();
+			
 		},
+		
 		msgError : function(){
+			$("#msg").html("");
 			$("#message").show();
 			$("#msg").append("Le jours doit etre compris entre 1 et 31");
 
 		},
 		conditionDays : function(){
+			$("#userInputDays").css("border-color","#ff7473");
+			$("#main").show();
+			$("#overlay").hide();
+			$("#msgOverlay").html("");
+			this.msgError();
+			
 
 		},
 		conditionYears : function(){
+			$("#userInputYears").css("border-color","#ff7473");
+			console.log("years");
+			$("#main").show();
+			$("#overlay").hide();
+			$("#msgOverlay").html("");
+			$("#msg").html("");
+			$("#message").show();
+			$("#msg").append("Renseigner l'année !");
+			
+		},
+		restart : function(){
+			$("#main").show();
+			$("#msgOverlay").html("");			
+			$("#overlay").hide();
+			this.ans = undefined;
+			console.log(this.an);
+			this.condition();
 
 		},
 
 	}
-app.init();
+	app.init();
 })();
-		
