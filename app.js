35f6a65e52244966002fa299b16c464755647052
LABORDE <dylan4047@gmail.com>
2016-10-26 16:55:23 +0200

    final version

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
 console.log("hello world");
-
 (function(){
-
 	var app = {
 		jours :undefined,
 		an : undefined,
+		userdate : undefined,	
 		date: undefined,
-		
 		init : function(){
 			app.watcher();
 			this.month();
-			console.log(this.jours);
-			console.log(this.jours)
 			$("#message").hide();
-
 			this.remove();
-			
 		},
 		watcher : function(){
 			var that = this ;
@@ -30,18 +24,14 @@ console.log("hello world");
 			var date = userdate.format("dddd");
 			$("#main").hide()
 			$("#overlay").show();
-			$("#msgOverlay").append(date);
+			$("#msgOverlay").html(date);
 			$("#message").hide();
 			$("#userInputDays").css("border-color","#47b8e0");
 			$("#userInputYears").css("border-color","#47b8e0");
 			this.condition();
-			console.log(date);
 		},
-		
-
 		days: function(){
 			this.jours = $("#userInputDays").val();
-			
 		},
 		years : function(){
 			this.an = $("#userInputYears").val();
@@ -52,58 +42,60 @@ console.log("hello world");
 
 		},
 		condition: function(){
+			userdate = moment([this.jours+this.mois+this.an],"DD-MM-YYYY")
+			date = userdate.format("dddd");
 			if (this.jours === undefined) {
 				this.conditionDays();
 			}
 			if (this.an === undefined) {
-				this.conditionYears();	
+				this.conditionYears();		
 			};
 			if (this.an === "") {
 				this.conditionYears();	
 			};
 			if (this.jours === undefined && this.an === undefined) {
-				console.log("haha");
 				$("#userInputDays").css("border-color","#ff7473");
 				$("#userInputYears").css("border-color","#ff7473");
 				$("#message").show();
 				$("#msg").html("");
-				$("#msg").append("erreur aucune valeur n'a été renseigné");
+				$("#msg").html("erreur aucune valeur n'a été renseigné");
 				$("#main").show();
 				$("#overlay").hide();
 			};
+			if (date === "Invalid date") {
+				$("#userInputDays").css("border-color","#ff7473");
+				$("#message").show();
+				$("#msg").html("");
+				$("#msg").html("Entrer une date valide");
+				$("#main").show();
+				$("#overlay").hide();
+			}
 
 		},
 		remove : function(){
 			$("#main").show();
 			$("#msgOverlay").html("");			
-			$("#overlay").hide();
-			
+			$("#overlay").hide();	
 		},
-		
 		msgError : function(){
 			$("#msg").html("");
 			$("#message").show();
-			$("#msg").append("Le jours doit etre compris entre 1 et 31");
-
+			$("#msg").html("Le jours doit etre compris entre 1 et 31");
 		},
 		conditionDays : function(){
 			$("#userInputDays").css("border-color","#ff7473");
 			$("#main").show();
 			$("#overlay").hide();
 			$("#msgOverlay").html("");
-			this.msgError();
-			
-
 		},
 		conditionYears : function(){
 			$("#userInputYears").css("border-color","#ff7473");
-			console.log("years");
 			$("#main").show();
 			$("#overlay").hide();
 			$("#msgOverlay").html("");
 			$("#msg").html("");
 			$("#message").show();
-			$("#msg").append("Renseigner l'année !");
+			$("#msg").html("Renseigner l'année !");
 			
 		},
 		restart : function(){
@@ -112,11 +104,8 @@ console.log("hello world");
 			$("#overlay").hide();
 			$("#userInputDays").empty();
 			this.ans = undefined;
-			console.log(this.an);
 			this.condition();
-
 		},
-
 	}
 	app.init();
 })();
